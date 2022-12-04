12596025f48393f7bd253e40810abfb5ee659e87
Entiti210 <85221876+Entiti210@users.noreply.github.com>
2022-12-05 01:26:05 +0700

    05/12 commit

--- src/config/db/thongke.js
+++ src/config/db/thongke.js
@@ -4,7 +4,12 @@ const db = require('./index');
 async function THONGKE() {
     try {
         const connection = await db.connect();
-        const result = await connection.request().query(`SELECT * FROM DONMUA`);
+        const result = await connection.request()
+            .query(`select ID_DONMUACT, DONMUA, TEN_KHACHHANG, VATPHAM, TEN_VATPHAM, TEN_STORE, SOLUONGVP, DONGIAVP, NGAYTHANG, GHICHU, TONGTIEN, NGAYGIAO, FEE
+        from DONMUA_CT a join DONMUA b on b.ID_DONMUA = a.DONMUA 
+        join KHACHHANG c on b.KHACHHANG = c.ID_KHACHHANG 
+        join VATPHAM d on a.VATPHAM = d.ID_VATPHAM 
+        join STORE e on b.STORE = e.ID_STORE ORDER BY NGAYTHANG DESC`);
         return result.recordsets[0];
     } catch (err) {
         console.log('Error: ', err);
